4ff36ad32abca03b12ba9f66cf20e05b97e6e08e
RedCupCrowe <valdemarkjohansen@gmail.com>
2021-09-29 09:57:27 +0200

    HelloWorld 8 times

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     System.out.println("Hello World");
     System.out.println("Hello World");
     System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    System.out.println("Hello World");
 
   }
 }
